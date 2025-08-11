7d4a7dcbd117e3506fffb5937e6e94ad57bac49b
parvaiz-ikior <136723580+parvaiz-bhat@users.noreply.github.com>
2025-08-11 14:02:23 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,7 @@
+# variables.tf
+
+variable "external_id" {
+  description = "The unique External ID provided by the CyFin platform for secure role assumption. You can find this on the 'Connect Account' page in CyFin."
+  type        = string
+  sensitive   = true # Marks the variable as sensitive to prevent it from being shown in logs.
+}
