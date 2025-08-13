bd8ac05430e59fd3ff45445f2048e9e39fa8ec54
parvaiz-bhat <parvaiz.bhat@ikior.com>
2025-08-13 09:32:23 +0530

    Finalized

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-# outputs.tf
+# C:\IKIOR\terraform-aws-cyfin-integration\outputs.tf (Corrected Version)
 
 output "cyfin_role_arn" {
   description = "The Amazon Resource Name (ARN) of the created IAM role for CyFin."
-  value       = module.iam_role_for_cyfin.iam_role_arn
+  value       = module.cyfin_assumable_role.iam_role_arn
 }
